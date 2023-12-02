ae206d3d0c9c2202fd453218683994a4341c0fff
ArminSchaefer <schaefer@politik.uni-mainz.de>
2023-12-02 17:53:59 +0100

    Changed default theme in _common.R

--- _common.R
+++ _common.R
@@ -15,17 +15,44 @@ knitr::opts_chunk$set(
 # load packages
 
 if(!require("pacman")) {install.packages("pacman");library(pacman)}
-p_load(here, palmerpenguins, tidyverse)
+p_load(here, rnaturalearth, tmap, palmerpenguins, democracyData, tidyverse)
 
 
-# create a theme
+# set a theme
 
-theme_vp <- function(...) theme(
-  title = element_text(color = "black", size = 10),
-  axis.text = element_text(color = "black", size = 10),
-  axis.title = element_text(color = "black",
-                            face = "bold",
-                            size = 10),
-  strip.text = element_text(color = "black",
-                            size = 12),
-  panel.grid.minor = element_blank())
+options(
+  tidyverse.quiet = TRUE,
+  propensity.quiet = TRUE,
+  tipr.verbose = FALSE,
+  htmltools.dir.version = FALSE,
+  width = 55,
+  digits = 4,
+  ggplot2.discrete.colour = ggokabeito::palette_okabe_ito(),
+  ggplot2.discrete.fill = ggokabeito::palette_okabe_ito(),
+  ggplot2.continuous.colour = "viridis",
+  ggplot2.continuous.fill = "viridis",
+  book.base_size = 12
+)
+
+library(ggplot2)
+
+theme_set(
+  theme_minimal(
+    base_size = getOption("book.base_size"),
+    base_family = getOption("book.base_family")
+  ) %+replace%
+    theme(
+      title = element_text(color = "black", 
+                           size = 10),
+      axis.text = element_text(color = "black", 
+                               size = 10),
+      axis.title = element_text(color = "black",
+                                face = "bold",
+                                size = 10),
+      strip.text = element_text(color = "black",
+                                size = 12),
+      panel.grid.minor = element_blank(),
+      legend.text = element_text(size = 8),
+      legend.key.size = unit(.5, 'cm')
+    )
+)
